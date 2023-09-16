ede98efd9817a9ba674ebdf0f36224f8ec98f7fd
Hongsik Choi <playhong16@gmail.com>
2023-09-16 23:14:50 +0900

    [FEAT]: ProfileView 생성

---
+++ UsingCoreData/Controllers/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  UsingCoreData
+//
+//  Created by playhong on 2023/09/16.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ UsingCoreData/Views/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  UsingCoreData
+//
+//  Created by playhong on 2023/09/16.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
